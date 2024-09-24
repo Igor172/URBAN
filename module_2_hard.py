7618e5adf89a54c5f64915f63c98d5ebe9bc33bf
Igor172 <155095940+Igor172@users.noreply.github.com>
2024-09-24 11:35:12 +0500

    23.09.2024

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,6 +3,8 @@ num = int(input('Введите число от 3 до 20: '))
 password = []
 for i in range(1, num):
     for j in range(1, num):
-        if num % (i + j) == 0:
+        if num % (i + j) == 0 and i <= j:
             password.append((i, j))
-print(*password)
+
+for pair in password:
+    print(*pair, end=' ')
