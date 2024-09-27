45aff239b2a8a1b093aacd9a7942fbefdbd81d67
Igor172 <155095940+Igor172@users.noreply.github.com>
2024-09-27 20:26:27 +0500

    27.09.2024 Внесена корректировка кода в код преподавателя

--- module_4/sortfunc.py
+++ module_4/sortfunc.py
@@ -1,5 +1,6 @@
 nums_1 = [28, 1, 5, 3, 82, 16]
-nums_2 = [5, 7, 47, 37, 27, 17]
+nums_2 = [5, 7, 47, 37, 27, 17, 89, 34]
+nums_3 = [96, 1, 48, 3, 59, 67, 5, 13, 90, 1]
 
 def bubble_sort(ls):
     '''Сортировка пузырьком'''
@@ -18,10 +19,26 @@ def selection_sort(ls):
         for j in range(i + 1, len(ls)):
             if ls[j] < ls[lowest]:
                 lowest = j
-        ls[j], ls[lowest] = ls[lowest], ls[j]
+        ls[i], ls[lowest] = ls[lowest], ls[i]
 
-bubble_sort(nums_2)
-print(nums_2)
+def insertion_sort(ls):
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls[j]
+            j -= 1
+        ls[j + 1] = key
+    return ls
+
+
+
+
+
+
+bubble_sort(nums_1)
+print(nums_1)
 selection_sort(nums_2)
 print(nums_2)
-
+insertion_sort(nums_3)
+print(nums_3)
