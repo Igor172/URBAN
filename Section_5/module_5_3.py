68befd2b3d88ab27318f0d5f2a42bc6c94bb1119
Igor172 <155095940+Igor172@users.noreply.github.com>
2024-10-04 20:34:39 +0500

    module_5_3 added

--- Section_5/module_5_3.py
+++ Section_5/module_5_3.py
@@ -1,10 +1,17 @@
-from gevent.subprocess import value
-
 
 class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        obj = object.__new__(cls)
+        cls.houses_history.append(args[0])
+        return obj
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
 
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -12,6 +19,10 @@ class House:
         elif 0 < new_floor <= self.number_of_floors:
             for i in range(1, new_floor + 1):
                 print(i)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
 
     def __len__(self):
         return self.number_of_floors
@@ -61,25 +72,14 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
