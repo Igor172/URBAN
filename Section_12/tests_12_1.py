d114b332cd83b23a06630d5d6248fc3518a9a99f
Igor172 <155095940+Igor172@users.noreply.github.com>
2024-12-10 23:42:44 +0500

    test_12_1 added (unittest)

---
+++ Section_12/tests_12_1.py
@@ -0,0 +1,67 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        runner_1 = Runner('Athlet_1')
+        for _ in range(10):
+            runner_1.walk()
+        self.assertEqual(runner_1.distance, 60) #Намеренно ввели бОльшее значение чем ожидалось, тесты показали место ошибки
+
+    def test_run(self):
+        runner_2 = Runner('Athlet_2')
+        for _ in range(10):
+            runner_2.run()
+        self.assertEqual(runner_2.distance, 100)
+
+    def test_challenge(self):
+        runner_3 = Runner('Athlet_3')
+        for _ in range(10):
+            runner_3.run()
+        runner_4 = Runner('Athlet_4')
+        for _ in range(10):
+            runner_4.walk()
+        self.assertNotEqual(runner_3, runner_4, msg='Атлеты прошли разную дистанцию')
+
+
+if __name__ == '__main__':
+
+    RunnerTest()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
