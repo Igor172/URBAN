2c9abdb78928e5f44f7268654257330a69b921f6
Igor172 <155095940+Igor172@users.noreply.github.com>
2024-12-21 16:28:40 +0500

    test_12_3 added with resolved problem

--- Section_12/Test_12_3/tests_12_1.py
+++ Section_12/Test_12_3/tests_12_1.py
@@ -21,21 +21,21 @@ class RunnerTest(unittest.TestCase):
 
     is_frozen = False
 
-    @unittest.skipIf(RunnerTest.is_frozen == True, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         runner_1 = Runner('Athlet_1')
         for _ in range(10):
             runner_1.walk()
         self.assertEqual(runner_1.distance, 50) #Намеренно ввели бОльшее значение чем ожидалось, тесты показали место ошибки
 
-    @unittest.skipIf(RunnerTest.is_frozen == True, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         runner_2 = Runner('Athlet_2')
         for _ in range(10):
             runner_2.run()
         self.assertEqual(runner_2.distance, 100)
 
-    @unittest.skipIf(RunnerTest.is_frozen == True, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         runner_3 = Runner('Athlet_3')
         for _ in range(10):
